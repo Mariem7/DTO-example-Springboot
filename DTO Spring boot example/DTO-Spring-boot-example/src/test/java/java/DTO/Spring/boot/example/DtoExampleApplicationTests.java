e65cf19638a07f195591f32b7b288b28f022da69
Mariem Neili <mariemneili7@gmail.com>
2022-06-03 15:46:18 +0100

    initialize project

---
+++ DTO Spring boot example/DTO-Spring-boot-example/src/test/java/java/DTO/Spring/boot/example/DtoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package java.DTO.Spring.boot.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
